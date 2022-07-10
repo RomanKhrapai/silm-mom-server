494e9d3e8bdf6740cdd559788ddc6e37de90619b
Pavel-Baturin <pavel.baturin.92@gmail.com>
2022-07-10 16:54:46 +0300

    add userSchema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 
+const regexpEmail = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
+
 const userSchema = Schema(
   {
     name: {
@@ -20,7 +22,17 @@ const userSchema = Schema(
       required: [true, "Email is required"],
       minlength: 3,
       maxlength: 254,
+      match: regexpEmail,
       unique: true,
+    },
+    height: { type: Number, min: 100, max: 250 },
+    age: { type: Number, min: 18, max: 100 },
+    currentWeight: { type: Number, min: 20, max: 500 },
+    desiredWeight: { type: Number, min: 20, max: 500 },
+    bloodType: {
+      type: Number,
+      enum: [1, 2, 3, 4],
+      default: 1,
     },
     token: {
       type: String,
@@ -31,12 +43,18 @@ const userSchema = Schema(
 );
 
 const joiRegisterSchema = Joi.object({
-  name: Joi.string().required().min(3).max(254).error(new Error("missing required name field")),
+  name: Joi.string()
+    .required()
+    .min(3)
+    .max(254)
+    .error(new Error("missing required name field")),
   email: Joi.string()
     .required()
     .error(new Error("missing required email field")),
   password: Joi.string()
-    .required().min(8).max(100)
+    .required()
+    .min(8)
+    .max(100)
     .error(new Error("missing required password field")),
 });
 
@@ -45,7 +63,9 @@ const joiLoginSchema = Joi.object({
     .required()
     .error(new Error("missing required email field")),
   password: Joi.string()
-    .required().min(8).max(100)
+    .required()
+    .min(8)
+    .max(100)
     .error(new Error("missing required password field")),
 });
 
