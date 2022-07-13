03bef62bef77f4a6b5bf8a55b4ab0b8dff5a3071
RomanKhrapai <hrapayr@gmail.com>
2022-07-13 22:43:46 +0300

    Create google auth

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,10 +2,14 @@ const register = require("./register");
 const login = require("./login");
 const logout = require("./logout");
 const refresh = require("./refresh");
+const googleAuth = require("./googleAuth");
+const googleRedirect = require("./googleRedirect")
 
 module.exports = {
   register,
   login,
   logout,
   refresh,
+  googleAuth,
+  googleRedirect
 };
