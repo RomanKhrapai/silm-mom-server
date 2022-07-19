b3b7bbd6e5b5f54c16fd9c0a86ffe1e776a7b99f
RomanKhrapai <hrapayr@gmail.com>
2022-07-20 00:42:23 +0300

    fix check invalid token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,30 +5,30 @@ const { User } = require("../models/user");
 const { ACCESS_SECRET_KEY } = process.env;
 
 const auth = async (req, res, next) => {
-  const { authorization = "" } = req.headers;
-  const [bearer, token] = authorization.split(" ");
+    const { authorization = "" } = req.headers;
+    const [bearer, token] = authorization.split(" ");
 
-  try {
-    if (bearer !== "Bearer") {
-      throw new Unauthorized("Not authorized");
+    try {
+        if (bearer !== "Bearer") {
+            throw new Unauthorized("Not authorized");
+        }
+        const { id } = jwt.verify(token, ACCESS_SECRET_KEY);
+        const user = await User.findById(id);
+        if (!user || !user.refreshToken) {
+            throw new Unauthorized("Not authorized");
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            res.status(400).json({ message: "Expired token" });
+            return;
+        } else if (error instanceof jwt.JsonWebTokenError) {
+            res.status(400).json({ message: "Invalid token!" });
+            return;
+        }
+        next(error);
     }
-    const { id } = jwt.verify(token, ACCESS_SECRET_KEY);
-    const user = await User.findById(id);
-    if (!user || !user.refreshToken) {
-      throw new Unauthorized("Not authorized");
-    }
-    req.user = user;
-    next();
-  } catch (error) {
-    if (error instanceof jwt.TokenExpiredError) {
-     res.status(400).json({message: "Expired token"});
-     return;
-    } else if (error instanceof jwt.JsonWebTokenError) {
-      res.status(400).json({ message: "Invalid token" });
-      return;
-    }
-    next(error);
-  }
 };
 
 module.exports = auth;
