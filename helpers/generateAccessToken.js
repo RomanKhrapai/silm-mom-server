9e41503af8ef28cffd94fadcd63ac75d83e171ba
RomanKhrapai <hrapayr@gmail.com>
2022-07-20 10:34:29 +0300

    prittier

--- helpers/generateAccessToken.js
+++ helpers/generateAccessToken.js
@@ -2,14 +2,14 @@ const jwt = require("jsonwebtoken");
 const { ACCESS_SECRET_KEY } = process.env;
 
 const generateAccessToken = (id) => {
-  const payloadAccess = {
-    id,
-    type: "access",
-  };
+    const payloadAccess = {
+        id,
+        type: "access",
+    };
 
-  return jwt.sign(payloadAccess, ACCESS_SECRET_KEY, {
-    expiresIn: "15m",
-  });
+    return jwt.sign(payloadAccess, ACCESS_SECRET_KEY, {
+        expiresIn: "15m",
+    });
 };
 
 module.exports = generateAccessToken;
